529e6fd2589f0a44166f1d96b9ac55a69a497182
Julio <juliobadilloguzman@gmail.com>
2020-10-29 15:54:32 -0500

    Math3d Methods

--- Math3D.cpp
+++ Math3D.cpp
@@ -1,1 +1,67 @@
-#include "3dMath.h"
+#include "Math3D.h"
+
+Math3D::Math3D()
+{
+
+}
+
+void Math3D::LoadIdentity(float M[][4])
+{
+  int i,j;
+  for(i=0;i<4;i++)
+    for(j=0;j<4;j++)
+      if(i==j)
+         M[i][j]=1;
+      else
+         M[i][j]=0;
+}
+
+void Math3D::translate(float x, float y, float z)
+{
+  LoadIdentity(T);
+  T[0][3]=x;
+  T[1][3]=y;
+  T[2][3]=z;
+}
+
+void Math3D::scale(float x, float y, float z)
+{
+
+}
+
+void Math3D::MultM(float M1[][4], float M2[][4], float Res[][4])
+{
+  float tmp[4][4];
+  int i,j,k;
+  for(i=0; i<4;i++)
+     for(j=0;j<4;j++){
+        tmp[i][j]=0;
+        for(k=0;k<4;k++)
+           tmp[i][j]+=M1[i][k]*M2[k][j];
+     }
+  for(i=0;i<4;i++)
+     for(j=0;j<4;j++)
+        Res[i][j] = tmp[i][j];
+}
+
+void Math3D::MatPoint(float m[][4], float p[3])
+{
+  float tmp[4];
+  int i,j;
+  for(i=0; i<3; i++)
+    { tmp[i] = p[i];
+      p[i] = 0;
+    }
+  tmp[3]=1;
+  for(i=0;i<3;i++)
+    for(j=0;j<4;j++)
+        p[i] += m[i][j]*tmp[j];
+}
+
+//Multiplica la matriz m por cada punto del objeto definido por la matriz p de size x 3
+void Math3D::MatObject(float m[][4], int size, float p[][3])
+{
+     int i;
+     for(i=0; i<size; i++)
+       MatPoint(m,p[i]);
+}
